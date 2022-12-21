4aa01f9cceccf4dea2c981223fe4d9545c52c3d9
Rawan Alharthi <s43800023@students.tu.edu.sa>
2022-12-21 10:45:31 +0300

    Initial Commit

---
+++ MusicLibrary/MusicLibraryApp.swift
@@ -0,0 +1,17 @@
+//
+//  MusicLibraryApp.swift
+//  MusicLibrary
+//
+//  Created by 라완 💕 on 27/05/1444 AH.
+//
+
+import SwiftUI
+
+@main
+struct MusicLibraryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
